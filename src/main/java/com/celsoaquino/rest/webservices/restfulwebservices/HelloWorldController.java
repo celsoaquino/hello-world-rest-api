2cba59a1a11e1f656fccdba95f06d4dc67fb5d44
celsoaquino <celso.aquino@ymail.com>
2019-12-14 14:17:23 -0300

    teste aws

--- src/main/java/com/celsoaquino/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/celsoaquino/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello Worldfsffsdfsdfdaf!";
+		return "Hello Worl - teste fffffffffff!";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
